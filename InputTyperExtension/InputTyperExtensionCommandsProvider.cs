ecd00f703b98c25311c38efb14ab1dbc40cf8e89
Corey Hayward <coreyhayward2@gmail.com>
2025-04-10 20:11:08 +0100

    Add project files.

---
+++ InputTyperExtension/InputTyperExtensionCommandsProvider.cs
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CommandPalette.Extensions;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace InputTyperExtension;
+
+public partial class InputTyperExtensionCommandsProvider : CommandProvider
+{
+    private readonly ICommandItem[] _commands;
+
+    public InputTyperExtensionCommandsProvider()
+    {
+        DisplayName = "Input Typer";
+        Icon = IconHelpers.FromRelativePath("Assets\\StoreLogo.png");
+        _commands = [
+            new CommandItem(new InputTyperExtensionPage()) { Title = DisplayName },
+        ];
+    }
+
+    public override ICommandItem[] TopLevelCommands()
+    {
+        return _commands;
+    }
+
+}
